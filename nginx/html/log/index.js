335cd503de2cf73f843bb82078d5d5b04dac6f72
Benjamin Kellermann <benjamin.kellermann@gmx.de>
2020-12-29 00:51:40 +0100

    group logfile

--- nginx/html/log/index.js
+++ nginx/html/log/index.js
@@ -39,6 +39,7 @@ const vueapp = new Vue({
 			{
 				label: 'State',
 				field: 'state',
+				hidden: true,
 				sortFn: function (x,y) {
 					var sortary = ['NEW', 'FETCHED', 'PAYED', 'DISCARDED'];
 					var a = sortary.indexOf(x);
@@ -85,32 +86,48 @@ const vueapp = new Vue({
 		prices: []
 	},
 	methods: {
+		parsePaymentLog(log) {
+			var o = {};
+			log.forEach(function(elem){
+				elem.day = elem.day.substring(0,10);
+				if (typeof(o[elem.state]) == 'undefined') {
+					var label;
+					switch(elem.state) {//['NEW', 'FETCHED', 'PAYED', 'DISCARDED']
+						case 'NEW':
+							label = 'New and unpaid orders';
+							break;
+						case 'FETCHED':
+							label = 'Fetched but unpaid orders';
+							break;
+						case 'PAYED':
+							label = 'Payed orders';
+							break;
+						case 'DISCARDED':
+							label = 'Discarded orders';
+							break;
+						default:
+							label = 'error'
+					}
+					o[elem.state] = {
+						mode: 'span',
+						label: label,
+						html: false,
+						children: [ elem ]
+					}
+				} else {
+					o[elem.state].children.push(elem);
+				}
+			});
+			this.payments = [];
+			for (const key in o) {
+				this.payments.push(o[key]);
+			}
+		},
 		getOpenPayments: function (event) {
 			lunch.then(
 				client => client.apis.Payments.getPayments({ community: this.community, from: this.userId, to: this.userId })
 			).then(
-				result => function(){
-					var p = JSON.parse(result.text).rows;
-					var today = new Date().toISOString().substring(0,10);
-					var prices = [];
-					var seen = {};
-					p.forEach(function(elem){
-						elem.day = elem.day.substring(0,10);
-						var n = {
-							day: elem.day,
-							shop: elem.shop,
-							meal: elem.meal,
-							price: elem.price
-						};
-						var key = elem.shop+elem.meal;
-						if (n.day === today && !seen[key]) {
-							seen[key] = true;
-							prices.push(n);
-						}
-					});
-					vueapp.payments = p;
-					vueapp.prices = prices;
-				}()
+				result => this.parsePaymentLog(JSON.parse(result.text).rows)
 			)
 		},
 		setOrderFetched: function (event) {
