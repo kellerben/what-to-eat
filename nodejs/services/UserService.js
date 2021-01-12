dfddfece94d4b475609772e6278c3978434cffc8
Benjamin Kellermann <benjamin.kellermann@gmx.de>
2021-01-13 00:39:59 +0100

    bugfix: add new userservice

---
+++ nodejs/services/UserService.js
@@ -0,0 +1,91 @@
+/* eslint-disable no-unused-vars */
+const Service = require('./Service');
+const mysql = require('mysql');
+const ws = require('../ws');
+
+/**
+* Get the payment information of a user
+*
+* community String The community string.
+* userId String The user
+* no response value expected for this operation
+* */
+const getPaymentInstructions = ({ community, userId }) => new Promise(
+	async (resolve, reject) => {
+		var stmt =
+			"SELECT paymentInstructions FROM users WHERE community = ? AND user = ?";
+		var sql = mysql.format(stmt, [community, userId]);
+		try {
+			Service.mysql_connection_pool.query(sql, function (err, rows, fields) {
+				if (err) {
+					console.error(err);
+					reject(Service.rejectResponse('Error while fetching paymentInstructions'));
+				} else {
+					resolve(Service.successResponse(rows[0]));
+				}
+			});
+		} catch (e) {
+			reject(Service.rejectResponse(
+				e.message || 'Invalid input',
+				e.status || 405,
+			));
+		}
+	},
+);
+/**
+* Set the payment information of the user
+*
+* community String The community string.
+* userId String The user
+* paymentInstructions PaymentInstructions
+* no response value expected for this operation
+* */
+const setPaymentInstructions = ({ community, userId, paymentInstructions }) => new Promise(
+	async (resolve, reject) => {
+		try {
+			var inserts = [paymentInstructions.paymentInstructions, community, userId];
+			var stmt =
+				"INSERT INTO users" +
+				" (paymentInstructions, community, user)" +
+				" VALUES (?, ?, ?)";
+
+			var sql = mysql.format(stmt, inserts);
+			Service.mysql_connection_pool.query(sql, function (err, rows, fields) {
+				if (err) {
+					if (err.errno == 1062) { // duplicate primary key
+						stmt =
+							"UPDATE users" +
+							" SET paymentInstructions = ?" +
+							" WHERE community = ? AND user = ?";
+						sql = mysql.format(stmt, inserts);
+						Service.mysql_connection_pool.query(sql, function (err, rows, fields) {
+							if (err) {
+								console.log('Error during update of paymentinfo: ', err);
+								reject(Service.rejectResponse('Error during the insertion of the payment instructions'));
+							} else {
+								ws.sendCommunity(community, "refreshOrders");
+								resolve(Service.successResponse('success'));
+							}
+						});
+					} else {
+						console.log('Error during insertion of paymentinfo: ', err);
+						reject(Service.rejectResponse('Error during the insertion of the payment instructions'));
+					}
+				} else {
+					ws.sendCommunity(community, "refreshOrders");
+					resolve(Service.successResponse('success'));
+				}
+			});
+		} catch (e) {
+			reject(Service.rejectResponse(
+				e.message || 'Invalid input',
+				e.status || 405,
+			));
+		}
+	},
+);
+
+module.exports = {
+	getPaymentInstructions,
+	setPaymentInstructions,
+};
